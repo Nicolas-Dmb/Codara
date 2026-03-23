e0d5adc00e228818d692a04162aafc740fb7c011
Nicolas_dmb <gyfeix@gmail.com>
2026-03-23 09:31:07 +0100

    feat/load env

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,9 @@
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = config::load_env::load_env();
+    println!("Database URL: {}", cfg.database_url);
+    println!("VPS Port: {}", cfg.vps_port);
+    println!("VPS IP: {}", cfg.vps_ip);
+    println!("VPS User: {}", cfg.vps_user);
 }
