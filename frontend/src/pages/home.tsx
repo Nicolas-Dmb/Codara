6e8a642e38136da24caad5e48672f26a4693236e
Nicolas_dmb <gyfeix@gmail.com>
2026-05-10 21:17:45 +0200

    feat/add config and design system

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home(){
+    return (
+        <div className="home">
+            <h1>Welcome to Aterminal</h1>
+            <p>Aterminal is a terminal emulator built with React and Electron. It is designed to be fast, lightweight, and customizable.</p>
+        </div>
+    )
+}
