2eba7336a2396eb40dcf60e93ca29bf759583833
Nicolas_dmb <gyfeix@gmail.com>
2026-05-10 21:33:19 +0200

    feat/ add icons on top left corner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { HashRouter as Router, Routes } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
 
   return (
     <Router>
       <Routes>
-
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   )
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,10 +1,9 @@
-
-
-export default function Home(){
-    return (
-        <div className="home">
-            <h1>Welcome to Aterminal</h1>
-            <p>Aterminal is a terminal emulator built with React and Electron. It is designed to be fast, lightweight, and customizable.</p>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <a href="/" className="inline-block px-8 py-4">
+        <img src="/icons.svg" alt="Aterminal" className="h-14 w-32" />
+      </a>
+    </div>
+  );
 }
